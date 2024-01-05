8ec0efcbf58b3e7fc1430c82c7aacddaee576a75
suhail-khowaja-dev <suhailkhowajadev@gmail.com>
2024-01-06 00:12:39 +0500

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\User;
+use Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Socialite;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+     */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    public function credentials(Request $request)
+    {
+        // $user = new User();
+        // $user->email = 'test@email.com';
+        // $user->password = Hash::make(1234);
+        // $user->status = 'active';
+        // $user->role = 'admin';
+        // $user->save();
+
+        return ['email' => $request->email, 'password' => $request->password, 'status' => 'active', 'role' => 'admin'];
+    }
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function redirect($provider)
+    {
+        // dd($provider);
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function Callback($provider)
+    {
+        $userSocial = Socialite::driver($provider)->stateless()->user();
+        $users = User::where(['email' => $userSocial->getEmail()])->first();
+        // dd($users);
+        if ($users) {
+            Auth::login($users);
+            return redirect('/')->with('success', 'You are login from ' . $provider);
+        } else {
+            $user = User::create([
+                'name' => $userSocial->getName(),
+                'email' => $userSocial->getEmail(),
+                'image' => $userSocial->getAvatar(),
+                'provider_id' => $userSocial->getId(),
+                'provider' => $provider,
+            ]);
+            return redirect()->route('home');
+        }
+    }
+}
